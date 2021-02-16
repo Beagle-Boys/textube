c38b16a3993417aa87865bf3fc453d7e54db475e
GrayHat12 <grayhathacks10@gmail.com>
2021-02-17 00:30:30 +0530

    service workers

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -60,11 +60,24 @@ registerRoute(
   ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
   // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
-    cacheName: 'images',
+    cacheName: 'server-images',
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
       new ExpirationPlugin({ maxEntries: 50 }),
+    ],
+  })
+);
+registerRoute(
+  // Add in any other file extensions or routing criteria as needed.
+  ({ url }) => url.pathname.endsWith('.png') || url.pathname.endsWith('.svg') || url.pathname.endsWith('.jpg') || url.pathname.endsWith('.gif'),
+  // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  new StaleWhileRevalidate({
+    cacheName: 'nonserver-images',
+    plugins: [
+      // Ensure that once this runtime cache reaches a maximum size the
+      // least-recently used images are removed.
+      new ExpirationPlugin({ maxEntries: 100 }),
     ],
   })
 );
